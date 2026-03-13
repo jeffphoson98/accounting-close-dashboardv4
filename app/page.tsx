02ac998a3920dcabf0a525343b316fd771185366
jeffphoson98 <jefferson@jnaccounting.co>
2026-03-13 14:02:57 -0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import MonthEndCloseCommandCenter from "@/components/MonthEndCloseCommandCenter"
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 p-6">
-      <MonthEndCloseCommandCenter />
-    </main>
-  )
-}
