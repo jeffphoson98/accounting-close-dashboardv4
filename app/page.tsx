7e414608c0ee8cf184c98d338d0f0ffe223829c0
jeffphoson98 <jefferson@jnaccounting.co>
2026-03-13 14:00:06 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import MonthEndCloseCommandCenter from "@/components/MonthEndCloseCommandCenter"
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-6">
+      <MonthEndCloseCommandCenter />
+    </main>
+  )
+}
